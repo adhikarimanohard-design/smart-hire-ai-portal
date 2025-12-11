f55b1f072ab1728302b66a38bea7a16e5ca9c4f1
adhikarimanohard-design <adhikarimanohard@gmail.com>
2025-12-11 22:37:40 +0530

    Update WebConfig.java

--- backend/src/main/java/com/smarthire/config/WebConfig.java
+++ backend/src/main/java/com/smarthire/config/WebConfig.java
@@ -1,4 +1,4 @@
-package main.java.com.smarthire.config;
+package.com.smarthire.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
