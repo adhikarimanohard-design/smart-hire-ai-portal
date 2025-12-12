b1555fd36e8ac216338e9f55995ea32694d4961c
adhikarimanohard-design <adhikarimanohard@gmail.com>
2025-12-12 10:50:34 +0530

    Update MongoConfig.java

--- backend/src/main/java/com/smarthire/config/MongoConfig.java
+++ backend/src/main/java/com/smarthire/config/MongoConfig.java
@@ -2,18 +2,15 @@ package com.smarthire.config;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
-import org.springframework.context.annotation.Bean; // New Import
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration // <--- KEEP THIS
+@Configuration 
 public class MongoConfig {
     
-    // Create the MongoClient bean using the environment variable
     @Bean
     public MongoClient mongoClient() {
         // This uses the MONGODB_URI variable you set on Render.
         return MongoClients.create(System.getenv("MONGODB_URI")); 
     }
-    
-    // You do not need getDatabaseName(), autoIndexCreation(), or AbstractMongoClientConfiguration.
 }
