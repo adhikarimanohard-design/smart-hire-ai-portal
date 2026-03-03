9a018ef7e1f4c17c2ae0cacd2521f7f377e7260f
adhikarimanohard-design <adhikarimanohard@gmail.com>
2026-03-03 18:28:20 +0530

    Update User.java

--- backend/src/main/java/com/smarthire/model/User.java
+++ backend/src/main/java/com/smarthire/model/User.java
@@ -143,4 +143,18 @@ public class User {
     public void setActive(boolean active) {
         this.active = active;
     }
+public String getPassword() { return password; }
+public void setPassword(String password) { this.password = password; }
+
+public String getFirstName() { return firstName; }
+public void setFirstName(String firstName) { this.firstName = firstName; }
+
+public String getLastName() { return lastName; }
+public void setLastName(String lastName) { this.lastName = lastName; }
+
+public String getResumeName() { return resumeName; }
+public void setResumeName(String resumeName) { this.resumeName = resumeName; }
+
+public boolean isResumeUploaded() { return resumeUploaded; }
+public void setResumeUploaded(boolean resumeUploaded) { this.resumeUploaded = resumeUploaded; }
 }
