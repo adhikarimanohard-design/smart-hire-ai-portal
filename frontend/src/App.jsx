207b7e509bf4db7ff332352b33ef17d788f2e9e5
adhikarimanohard-design <adhikarimanohard@gmail.com>
2026-07-17 19:46:16 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import './index.css';
 /* ================================
    CONFIG
 ================================ */
-const API_BASE = 'https://smart-hire-ai-portal-1-nwcf.onrender.com';
+const API_BASE = 'https://smart-hire-ai-portal-2-52o9.onrender.com';
 /* ================================
    COMPANY EMOJIS
 ================================ */
